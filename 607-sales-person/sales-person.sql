1c8cde9759673a72af550f81a659f1fd4b193130
Chester <67884903+ChesterChangLiu@users.noreply.github.com>
2025-07-20 00:17:05 -0400

    Time: 1339 ms (66.20%) | Memory: 0B (100.00%) - LeetSync

--- 607-sales-person/sales-person.sql
+++ 607-sales-person/sales-person.sql
@@ -7,3 +7,6 @@ WHERE s.sales_id NOT IN (
     JOIN Company c ON o.com_id = c.com_id
     WHERE c.name = 'RED'
 );
+#First identify which salespeople DID have orders with RED company
+#Then exclude those salespeople from the full list of salespeople
+#Return the remaining salespeople who never had orders with RED company
