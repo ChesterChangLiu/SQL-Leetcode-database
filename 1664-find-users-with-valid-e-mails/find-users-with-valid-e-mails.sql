363b6097474a22b109be9fada69483f96efd0705
Chester <67884903+ChesterChangLiu@users.noreply.github.com>
2025-07-28 21:59:56 -0400

    Time: 724 ms (43.40%) | Memory: 0B (100.00%) - LeetSync

--- 1664-find-users-with-valid-e-mails/find-users-with-valid-e-mails.sql
+++ 1664-find-users-with-valid-e-mails/find-users-with-valid-e-mails.sql
@@ -1,4 +1,9 @@
 SELECT *
 FROM Users
 WHERE 
-   REGEXP_LIKE(mail, '^[A-Za-z][A-Za-z0-9._-]*@leetcode\\.com$', 'c');
+   REGEXP_LIKE(mail, '^[A-Za-z][A-Za-z0-9._-]*@leetcode\\.com$','c');
+
+# REGEXP_LIKE() - Oracle's function for regular expression matching
+# First parameter: mail - the column to test
+# Second parameter: '^[A-Za-z][A-Za-z0-9._-]*@leetcode\\.com$' - the regex pattern
+# Third parameter: 'c' - the match parameter for case-sensitive matching
